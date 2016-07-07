f4a4b12e2291b6db189ffcb536295625d823cb6c
Victor Ivanov <Bublyash@users.noreply.github.com>
2016-07-07 15:39:57 +0400

    Add files via upload

---
+++ server.rb
@@ -0,0 +1,158 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width">
+  <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1" media="(device-height: 568px)">
+  <meta http-equiv="Content-Language" content="en">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+    <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats">
+  <meta name="theme-color" content="#f5f5f5">
+
+      <meta name="google-analytics" content="UA-3769691-2">
+
+<meta content="collector.githubapp.com" name="octolytics-host" /><meta content="github" name="octolytics-app-id" /><meta content="58C88964:1251:ECC7D40:577E14F2" name="octolytics-dimension-request_id" /><meta content="20319586" name="octolytics-actor-id" /><meta content="Bublyash" name="octolytics-actor-login" /><meta content="4724fea17ab317ea6463b15f23c0b4dd0cdc6b673bdfe7c3b2334973158379d4" name="octolytics-actor-hash" />
+<meta content="mobile" name="octolytics-dimension-device" />
+<meta content="9919" name="octolytics-dimension-user_id" /><meta content="github" name="octolytics-dimension-user_login" /><meta content="8445877" name="octolytics-dimension-repository_id" /><meta content="github/platform-samples" name="octolytics-dimension-repository_nwo" /><meta content="true" name="octolytics-dimension-repository_public" /><meta content="false" name="octolytics-dimension-repository_is_fork" /><meta content="8445877" name="octolytics-dimension-repository_network_root_id" /><meta content="github/platform-samples" name="octolytics-dimension-repository_network_root_nwo" />
+
+<meta content="/&lt;user-name&gt;/&lt;repo-name&gt;/blob/show" data-pjax-transient="true" name="analytics-location" />
+
+
+
+  <meta class="js-ga-set" name="dimension1" content="Logged In">
+    <meta class="js-ga-set" name="dimension3" content="mobile">
+
+
+
+
+  <title>GitHub · Where software is built</title>
+
+  <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/mobile-ecb39ea1acad240e8e798cedcc1edfbbd8cafb043f9ce10a0de48353069c1219.css" integrity="sha256-7LOeoaytJA6OeYztzB7fu9jK+wQ/nOEKDeSDUwacEhk=" media="all" rel="stylesheet" />
+  
+</head>
+<body class="">
+  <header class="nav-bar">
+    <div class="nav-bar-inner ">
+      <button class="header-button header-nav-button touchable js-show-global-nav" data-ga-click="Mobile, tap, location:header; text:Hamburger">
+        <svg aria-hidden="true" class="octicon octicon-three-bars" height="24" version="1.1" viewBox="0 0 12 16" width="18"><path d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path></svg>
+      </button>
+
+      <div class="nav-bar-title-text">
+              <svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+
+    <strong><a href="/github">github</a></strong>
+    /
+    <strong><a href="/github/platform-samples">platform-samples</a></strong>
+
+      </div>
+
+      
+
+    </div>
+
+
+    <nav class="nav-bar-tabs repo-nav-bar-tabs">
+      <ul>
+            <li>
+              <a href="/" data-ga-click="Mobile, tap, location:header; text:Dashboard">
+                <svg aria-hidden="true" class="octicon octicon-dashboard" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M9 5H8V4h1v1zm4 3h-1v1h1V8zM6 5H5v1h1V5zM5 8H4v1h1V8zm11-5.5l-.5-.5L9 7c-.06-.02-1 0-1 0-.55 0-1 .45-1 1v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-.92l6-5.58zm-1.59 4.09c.19.61.3 1.25.3 1.91 0 3.42-2.78 6.2-6.2 6.2-3.42 0-6.21-2.78-6.21-6.2 0-3.42 2.78-6.2 6.2-6.2 1.2 0 2.31.34 3.27.94l.94-.94A7.459 7.459 0 0 0 8.51 1C4.36 1 1 4.36 1 8.5 1 12.64 4.36 16 8.5 16c4.14 0 7.5-3.36 7.5-7.5 0-1.03-.2-2.02-.59-2.91l-1 1z"></path></svg>
+                Dashboard
+              </a>
+            </li>
+          <li>
+            <a href="/explore" data-ga-click="Mobile, tap, location:header; text:Explore">
+              <svg aria-hidden="true" class="octicon octicon-telescope" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 9l3 6h-1l-2-4v5H7v-6l-2 5H4l2-5 2-1zM7 0H6v1h1V0zM5 3H4v1h1V3zM2 1H1v1h1V1zM.63 9a.52.52 0 0 0-.16.67l.55.92c.13.23.41.31.64.2l1.39-.66-1.16-2-1.27.86.01.01zm7.89-5.39l-5.8 3.95L3.95 9.7l6.33-3.03-1.77-3.06h.01zm4.22 1.28l-1.47-2.52a.51.51 0 0 0-.72-.17l-1.2.83 1.84 3.2 1.33-.64c.27-.13.36-.44.22-.7z"></path></svg>
+              Explore
+            </a>
+          </li>
+            <li>
+              <a href="/Bublyash" data-ga-click="Mobile, tap, location:header; text:User avatar">
+                <img alt="@Bublyash" class="avatar" height="16" src="https://avatars3.githubusercontent.com/u/20319586?v=3&amp;s=32" width="16" />
+                Profile
+              </a>
+            </li>
+        <li>
+            <a href="/logout" data-ga-click="Mobile, tap, location:header; text:Sign out">
+              <svg aria-hidden="true" class="octicon octicon-sign-out" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M12 9V7H8V5h4V3l4 3-4 3zm-2 3H6V3L2 1h8v3h1V1c0-.55-.45-1-1-1H1C.45 0 0 .45 0 1v11.38c0 .39.22.73.55.91L6 16.01V13h4c.55 0 1-.45 1-1V8h-1v4z"></path></svg>
+              Sign out
+            </a>
+        </li>
+            <li class="section-title">This repository</li>
+    <li><a href="/github/platform-samples?files=1"><svg aria-hidden="true" class="octicon octicon-code" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M9.5 3L8 4.5 11.5 8 8 11.5 9.5 13 14 8 9.5 3zm-5 0L0 8l4.5 5L6 11.5 2.5 8 6 4.5 4.5 3z"></path></svg> Code</a></li>
+      <li><a href="/github/platform-samples/issues"><svg aria-hidden="true" class="octicon octicon-issue-opened" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg> Issues</a></li>
+    <li><a href="/github/platform-samples/pulls"><svg aria-hidden="true" class="octicon octicon-git-pull-request" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0 0 10 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v6.56A1.993 1.993 0 0 0 2 15a1.993 1.993 0 0 0 1-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg> Pull Requests</a></li>
+    <li><a href="/github/platform-samples/pulse"><svg aria-hidden="true" class="octicon octicon-pulse" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M11.5 8L8.8 5.4 6.6 8.5 5.5 1.6 2.38 8H0v2h3.6l.9-1.8.9 5.4L9 8.5l1.6 1.5H14V8z"></path></svg> Pulse</a></li>
+
+      </ul>
+    </nav>
+  </header>
+
+    
+
+
+  <div id="js-flash-container">
+</div>
+
+
+        
+
+<div class="breadcrumb blob-breadcrumb">
+  <label for="blob-history-checkbox" class="blob-history-label">
+    <svg aria-hidden="true" class="octicon octicon-history" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 13H6V6h5v2H8v5zM7 1C4.81 1 2.87 2.02 1.59 3.59L0 2v4h4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7s7-3.14 7-7-3.14-7-7-7z"></path></svg>
+  </label>
+  <span class="filetype-icon"><svg aria-hidden="true" class="octicon octicon-file-text" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg></span>
+  <span class="js-path-segment"><a href="/github/platform-samples/tree/master/hooks"><span>hooks</span></a></span><span class="separator">/</span><span class="js-path-segment"><a href="/github/platform-samples/tree/master/hooks/ruby"><span>ruby</span></a></span><span class="separator">/</span><span class="js-path-segment"><a href="/github/platform-samples/tree/master/hooks/ruby/configuring-your-server"><span>configuring-your-server</span></a></span><span class="separator">/</span><strong class="final-path">server.rb</strong>
+</div>
+
+<input id="blob-history-checkbox"
+       class="js-blob-history-checkbox blob-history-checkbox"
+       type="checkbox"
+       data-url="/github/platform-samples/latest_commit/master/hooks/ruby/configuring-your-server/server.rb">
+
+<div class="blob-history">
+  <a href="/github/platform-samples/commits/master/hooks/ruby/configuring-your-server/server.rb" class="js-blob-history-link">
+    Loading latest commit…
+</a></div>
+
+  <div class="blob-file-content js-file-line-container">
+    <div class="highlighted-blob tab-size" data-tab-size="8"><div class="code-body highlight"><pre><div class='line js-file-line' id='LC1'><span class="pl-k">require</span> <span class="pl-s"><span class="pl-pds">&#39;</span>sinatra<span class="pl-pds">&#39;</span></span></div><div class='line js-file-line' id='LC2'><span class="pl-k">require</span> <span class="pl-s"><span class="pl-pds">&#39;</span>json<span class="pl-pds">&#39;</span></span></div><div class='line js-file-line' id='LC3'><br></div><div class='line js-file-line' id='LC4'>post <span class="pl-s"><span class="pl-pds">&#39;</span>/payload<span class="pl-pds">&#39;</span></span> <span class="pl-k">do</span></div><div class='line js-file-line' id='LC5'>&nbsp;&nbsp;push <span class="pl-k">=</span> <span class="pl-c1">JSON</span>.parse(request.body.read)</div><div class='line js-file-line' id='LC6'>&nbsp;&nbsp;puts <span class="pl-s"><span class="pl-pds">&quot;</span>I got some JSON: <span class="pl-pse">#{</span><span class="pl-s1">push.inspect</span><span class="pl-pse"><span class="pl-s1">}</span></span><span class="pl-pds">&quot;</span></span></div><div class='line js-file-line' id='LC7'><span class="pl-k">end</span></div></pre></div></div>
+  </div>
+
+
+
+  <footer class="clearfix">
+    <div class="container">
+      <a href="#"><svg aria-hidden="true" class="octicon octicon-mark-github" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
+
+      <ul class="clearfix">
+        <li>
+          <!-- </textarea> --><!-- '"` --><form accept-charset="UTF-8" action="/site/mobile_preference" class="js-mobile-preference-form" data-form-nonce="32490ac7e3692617b7ca1b2dbf4b0c81fb5c19e6" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="rULkrcgrGfLt3ew0XFjy2obEkSrct4pU640yQqvUaKX4fEU0QIjJOcu11Q3Q59QmeyeDFYootPjw8qVtNlxMAA==" /></div>
+            <input type="hidden" name="mobile" value="false">
+            <input type="hidden" name="anchor" class="js-mobile-preference-anchor-field">
+
+            <button type="submit" class="switch-to-desktop" data-ga-click="Mobile, switch to desktop, switch button">
+              Desktop version
+            </button>
+</form>        </li>
+
+      </ul>
+    </div>
+  </footer>
+  
+  <script async="async" crossorigin="anonymous" integrity="sha256-0IAI+R/dG1SJWNQ+3lcZmTWgWVf0y0vnrjb5KFI8aFs=" src="https://assets-cdn.github.com/assets/mobile-d08008f91fdd1b548958d43ede57199935a05957f4cb4be7ae36f928523c685b.js"></script>
+</body>
+</html>
+
